214cdb78980ee136fe966e189e5fe9ebf14ac9aa
Elon Salfati <elon@salfati.io>
2022-08-16 13:05:07 +0300

    feat: added a utility function to add values to simply add values to existing tags

--- log.go
+++ log.go
@@ -10,61 +10,68 @@ import (
 
 // options for publishing logsnag events
 type PublishOptions struct {
-    Channel string `json:"channel"`
-    Event string `json:"event"`
-    Description string `json:"description,omitempty"`
-    Icon string `json:"icon,omitempty"`
-    Tags *Tags `json:"tags,omitempty"`
-    Notify bool `json:"notify,omitempty"`
+	Channel     string `json:"channel"`
+	Event       string `json:"event"`
+	Description string `json:"description,omitempty"`
+	Icon        string `json:"icon,omitempty"`
+	Tags        *Tags  `json:"tags,omitempty"`
+	Notify      bool   `json:"notify,omitempty"`
 }
 
 // extend the PublishOptions struct with the Porject ID
 type publishOptionsRequest struct {
-    // inherit PublishOptions
-    PublishOptions
+	// inherit PublishOptions
+	PublishOptions
 
-    // extend with Project ID
-    Project string `json:"project"`
+	// extend with Project ID
+	Project string `json:"project"`
 }
 
 // tag type
 type Tags map[string]interface{}
 
+// extend the tags with additional values
+func (t *Tags) Add(key string, value interface{}) {
+	if t == nil {
+		t = &Tags{}
+	}
+	(*t)[key] = value
+}
+
 // Publish a new event to LogSnag
 // options: LogSnag options
 func (l *LogSnag) Publish(ctx context.Context, options *PublishOptions) error {
-    // add the project id to the options
-    optionsWithProject := &publishOptionsRequest{
-        PublishOptions: *options,
-        Project: l.project,
-    }
+	// add the project id to the options
+	optionsWithProject := &publishOptionsRequest{
+		PublishOptions: *options,
+		Project:        l.project,
+	}
 
-    // get request body from options
-    reqBody, err := json.Marshal(optionsWithProject)
-    if err != nil {
-        return err
-    }
+	// get request body from options
+	reqBody, err := json.Marshal(optionsWithProject)
+	if err != nil {
+		return err
+	}
 
-    req, err := http.NewRequestWithContext(ctx, "POST", LOGSNAG_ENDPOINT+"/log", bytes.NewBuffer(reqBody))
-    if err != nil {
-        return err
-    }
+	req, err := http.NewRequestWithContext(ctx, "POST", LOGSNAG_ENDPOINT+"/log", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return err
+	}
 
-    // add request headers
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("Authorization", "Bearer "+l.token)
+	// add request headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+l.token)
 
-    // send request
-    resp, err := l.client.Do(req)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	// send request
+	resp, err := l.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("LogSnag error code: %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("LogSnag error code: %s", resp.Status)
+	}
 
-    return nil
+	return nil
 }
-
